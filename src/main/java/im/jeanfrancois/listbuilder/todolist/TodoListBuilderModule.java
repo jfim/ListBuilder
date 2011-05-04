39a1159903ff06ad3d69ff45d91bbbd2fee8dc33
Jean-Francois Im <jeanfrancois.im@gmail.com>
2011-05-04 01:11:17 -0400

    Made font selection work

--- src/main/java/im/jeanfrancois/listbuilder/todolist/TodoListBuilderModule.java
+++ src/main/java/im/jeanfrancois/listbuilder/todolist/TodoListBuilderModule.java
@@ -2,7 +2,6 @@ package im.jeanfrancois.listbuilder.todolist;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import im.jeanfrancois.listbuilder.print.TitledPrintable;
 import net.guts.gui.application.AppLifecycleStarter;
 import net.guts.gui.resource.Resources;
 
@@ -18,6 +17,7 @@ public class TodoListBuilderModule extends AbstractModule {
     protected void configure() {
         bind(AppLifecycleStarter.class).to(AppLifecycle.class);
         bind(Printable.class).to(TodoListPrintable.class).in(Scopes.SINGLETON);
+        bind(TodoListModel.class).in(Scopes.SINGLETON);
         Resources.bindRootBundle(binder(), TodoListBuilderModule.class, "todolistbuilder");
     }
 }
--- src/main/java/im/jeanfrancois/listbuilder/todolist/TodoListBuilderPanel.java
+++ src/main/java/im/jeanfrancois/listbuilder/todolist/TodoListBuilderPanel.java
@@ -2,6 +2,7 @@ package im.jeanfrancois.listbuilder.todolist;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.l2fprod.common.swing.JFontChooser;
 import im.jeanfrancois.listbuilder.ui.PreviewAndPrintPanel;
 import net.miginfocom.swing.MigLayout;
 import org.jdesktop.beansbinding.AutoBinding;
@@ -9,6 +10,8 @@ import org.jdesktop.beansbinding.BeanProperty;
 import org.jdesktop.beansbinding.Bindings;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -22,18 +25,20 @@ public class TodoListBuilderPanel extends JPanel {
     private JLabel titleTextLabel = new JLabel();
     private JTextField titleTextTextField = new JTextField();
     private JLabel fontLabel = new JLabel();
+    private JLabel fontDisplayLabel = new JLabel();
     private JButton fontChooseButton = new JButton();
 
     private TodoListModel todoListModel;
 
     @Inject
-    public TodoListBuilderPanel(TodoListModel todoListModel, final PreviewAndPrintPanel previewAndPrintPanel) {
+    public TodoListBuilderPanel(final TodoListModel todoListModel, final PreviewAndPrintPanel previewAndPrintPanel) {
         this.todoListModel = todoListModel;
 
         setLayout(new MigLayout("", "[grow]", "[][grow, fill]"));
-        add(titleTextLabel, "split 4");
+        add(titleTextLabel, "split");
         add(titleTextTextField);
         add(fontLabel, "gap unrelated");
+        add(fontDisplayLabel);
         add(fontChooseButton);
 
         add(previewAndPrintPanel, "newline, grow, span");
@@ -50,7 +55,23 @@ public class TodoListBuilderPanel extends JPanel {
         BeanProperty<JSpinner, Integer> spinnerValueIntProperty = BeanProperty.create("value");
 
         BeanProperty<TodoListModel, String> todoListModelTitleTextProperty = BeanProperty.create("titleText");
+        BeanProperty<TodoListModel, String> fontNameProperty = BeanProperty.create("font.name");
 
         Bindings.createAutoBinding(AutoBinding.UpdateStrategy.READ_WRITE, todoListModel, todoListModelTitleTextProperty, titleTextTextField, textFieldTextProperty).bind();
+        final AutoBinding<TodoListModel, String, JLabel, String> fontDisplayBinding = Bindings.createAutoBinding(AutoBinding.UpdateStrategy.READ_WRITE, todoListModel, fontNameProperty, fontDisplayLabel, labelTextProperty);
+        fontDisplayBinding.bind();
+
+        fontChooseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFontChooser fontChooser = new JFontChooser();
+                fontChooser.setSelectedFont(todoListModel.getFont());
+                fontChooser.showFontDialog(TodoListBuilderPanel.this,
+                        "Pick a font");
+                todoListModel.setFont(fontChooser.getSelectedFont());
+                fontDisplayBinding.unbind();
+                fontDisplayBinding.bind();
+            }
+        });
     }
 }
